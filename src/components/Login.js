0a239af6d9058d4a870b5499566122db9840abe1
Vedant0370 <vedant.0370@gmail.com>
2024-03-06 17:49:46 +0530

    update App.js and Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Login.css';
-import axios from 'axios';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
